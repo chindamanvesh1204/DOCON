081f20895bbb714d3c7d3578e34b74c1725833e4
chindamanvesh1204 <chindamanvesh@gmail.com>
2022-06-29 00:06:48 +0530

    added comments abc

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -24,6 +24,7 @@ export class LoginpageComponent implements OnInit {
     password:new FormControl ('', [Validators.required,
                                    Validators.minLength(5),
                                    Validators.maxLength(10),
+                                   //abcd//
                                      
                                                 
                                   
